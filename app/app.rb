fbd56be73920c46674dcd480db07115ca6eb280c
arnoldmanzano <arnold.manzano@gmail.com>
2016-02-10 13:47:51 +0000

    adding tags

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require_relative 'models/link'
+require_relative 'data_mapper_setup'
 
 ENV['RACK_ENV'] ||= 'development'
 
@@ -20,7 +21,11 @@ class Bookmark < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(title: params[:title], url: params[:url])
+    link = Link.create(title: params[:title], url: params[:url])
+    tag = Tag.create(name: params[:tag])
+    link.tags << tag
+    link.save
+    # LinkTag.create(:link => link, :tag => tag)
     redirect '/links'
   end
 
--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,7 +1,10 @@
 feature 'Viewing Links' do
 
   scenario '-> users can view the list of links on the links page' do
-    Link.create(url:'http://makersacademy.com', title:'Makers Academy')
+    link = Link.create(url:'http://makersacademy.com', title:'Makers Academy')
+    tag = Tag.create(name: 'education')
+    link.tags << tag
+    link.save
     visit '/links'
     expect(page.status_code).to eq 200
 
